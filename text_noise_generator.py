132b2a85f219b74aa6027dcf12163f5eef5fe029
Philin_Lemo <philinlemo@gmail.com>
2026-08-22 11:39:14 +0300

    Update text_noise_generator.py

--- text_noise_generator.py
+++ text_noise_generator.py
@@ -1,28 +1,8 @@
-#!/usr/bin/env python
-
-import random
-import time
-import argparse
-import curses
-
-marks_table = []
-colors = {"black": "\u001b[30m", "red": "\u001b[31m", "green": "\u001b[32m", "yellow": "\u001b[33m", "blue": "\u001b[34m", "magenta": "\u001b[35m", "cyan": "\u001b[36m", "white": "\u001b[37m", "light_black": "\u001b[90m", "light_red": "\u001b[91m", "light_green": "\u001b[92m", "light_yellow": "\u001b[93m", "light_blue": "\u001b[94m", "light_magenta": "\u001b[95m", "light_cyan": "\u001b[96m", "light_white": "\u001b[97m"}
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--sleep-time", help = "Time between printing characters (default: 0.05)", type = float, default = 0.05)
-parser.add_argument("-max", "--max-len", help = "Maximum word length (default: 12)", type = int, default = 12)
-parser.add_argument("-min", "--min-len", help = "Minimum word length (default: 3)", type = int, default = 3)
-parser.add_argument("-a", "--alphabet", help = "Alphabet used (en/ru or a string of custom characters) (default: en)", type = str, default = "abcdefghijklmnopqrstuvwxyz")
-parser.add_argument("--text-color", help = "Text color (default: white)", type = str, default = "white")
-parser.add_argument("-v", "--filling-void", help = "Frequency of spaces (default: 12)", type = int, default = 12)
-parser.add_argument("-p", "--filling-points", help = "Frequency of periods (default: 5)", type = int, default = 5)
-parser.add_argument("-c", "--filling-commas", help = "Frequency of commas (default: 3)", type = int, default = 3)
-parser.add_argument("-q", "--filling-question", help = "Frequency of question marks (default: 1)", type = int, default = 1)
-parser.add_argument("-e", "--filling-exclamation-marks", help = "Frequency of exclamation marks (default: 1)", type = int, default = 1)
-parser.add_argument("-s", "--spaces-after-punctuation-marks", help = "Spaces after punctuation marks (default: True)(if started with -s or an unabridged argument, then False)", action="store_false", default = True)
-
+parser.add_argument("--cursor", help = "Cursor type (default: 1)(0 - none, 1 - thin, 2 - wide)", type=int, default = 1)
+#Line to shorten binding of argument to variable
 args = parser.parse_args()
 
+#Declaration of variables from arguments
 sleep_time = args.sleep_time
 max_len = args.max_len
 min_len = args.min_len
@@ -34,15 +14,19 @@ filling_commas = args.filling_commas
 filling_question = args.filling_question
 filling_exclamation_marks = args.filling_exclamation_marks
 spaces_after_punctuation_marks = args.spaces_after_punctuation_marks
+cursor = args.cursor
 
+#For switching language
 if alphabet == "ru" :
     alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 
+#For spaces after punctuation marks
 if spaces_after_punctuation_marks == True :
     spaces_after_punctuation_marks = " "
 if spaces_after_punctuation_marks == False :
     spaces_after_punctuation_marks = ""
 
+#These 4 "for" loops are for configuring the frequency of punctuation marks
 for i in range(filling_void) :
     marks_table.append(" ")
 
@@ -58,20 +42,34 @@ for i in range(filling_question) :
 for i in range(filling_exclamation_marks) :
     marks_table.append("!" + spaces_after_punctuation_marks)
 
+#Main function
+#(stdscr) - pass the terminal window
 def main(stdscr) :
+    #Creating TUI interface
     curses.start_color()
     curses.use_default_colors()
-    screen = curses.initscr()
+    screen = curses.initscr() 
+    #Changing cursor appearance
+    curses.curs_set(cursor)
 
+    #Outputting text noise
+    #"try" - to prevent an error from appearing after stopping the program
     try :
         while True :
+            #Creating a word with length in the range from "min_len" to "max_len"
             for i in range(random.randint(min_len,max_len)) :
                 print(colors[text_color] + random.choice(alphabet), end="", flush=True)
+                #Delay
                 time.sleep(sleep_time)
+            #Selecting a random punctuation mark
+            #Chance depends on the number of characters added in lines 56 - 70
             if marks_table != [] :
                 print(colors[text_color] + random.choice(marks_table), end="", flush=True)
+                #Delay
                 time.sleep(sleep_time)
+    #Closing "try"
     except :
         pass
 
+#Closing TUI
 curses.wrapper(main)
